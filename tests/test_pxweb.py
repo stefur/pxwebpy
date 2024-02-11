39ecd0dbf47984cf85d0aa94691118e2234d9068
stefur <stefan@furne.net>
2024-02-11 15:07:31 +0100

    More rigorous testing
    - Added JSON responses for queries to PxWeb API's (Norway, Greenland, Finland) with parsed results for more thourough testing
    - Updated tests to also reflect changes in query (string rather than Path objects)

--- tests/test_pxweb.py
+++ tests/test_pxweb.py
@@ -1,6 +1,5 @@
 """Tests"""
 import json
-from pathlib import Path
 
 import pytest
 from pxwebpy import PxWeb
@@ -52,20 +51,30 @@ QUERY = """
   "response": {
     "format": "json-stat2"
   }
-}"""
+}
+"""
 
 
-def test_query_setter_with_json_string():
-    """The query should also be able to handle a JSON string"""
+def test_query_setter_with_string():
+    """The query should be able to handle a string representing a JSON structure"""
     pxweb = PxWeb(url=URL, query=QUERY, autofetch=False)
     json_query = json.loads(QUERY)
     assert pxweb.query == json_query
 
 
-def test_query_setter_with_invalid_json_string():
+def test_query_setter_with_file():
+    """The query should be able to handle a string representing a path to a file containing a JSON structure"""
+    pxweb = PxWeb(url=URL, query="tests/valid_query.json")
+    with open("tests/valid_query.json", mode="r", encoding="utf-8") as read_file:
+        json_query = json.load(read_file)
+
+    assert pxweb.query == json_query
+
+
+def test_query_setter_with_invalid_json():
     """Invalid query should produce an error"""
     with pytest.raises(ValueError):
-        PxWeb(url=URL, query=Path("tests/invalid_query.json"))
+        PxWeb(url=URL, query="tests/invalid_query.json")
 
 
 def test_query_setter_with_invalid_type():
@@ -86,3 +95,31 @@ def test_get_data_failure():
     """Invalid URL should raise a ValueError"""
     with pytest.raises(ValueError):
         PxWeb(url="invalid_url", query=QUERY)
+
+
+def test_mock_responses():
+    """This test checks functionality against mock Px Web API responses"""
+    with open("tests/queries.json", "r") as queries:
+        query_data = json.load(queries)
+
+    for query in query_data["queries"]:
+        url = query["url"]
+        query_params = query["query"]
+        expected_response = query["expected_response"]
+        expected_result = query["expected_result"]
+
+        mock_api = PxWeb("api.example.com", QUERY, autofetch=False)
+
+        # Load the expected response data from the referenced file
+        with open(expected_response, mode="r", encoding="utf-8") as response_file:
+            mock_response = json.load(response_file)
+
+        unpacked_response = mock_api._unpack_data(mock_response)
+
+        with open(expected_result, "r") as result_file:
+            mock_result = json.load(result_file)
+
+        # Compare the unpacked response wi data with the mock r response data
+        assert (
+            unpacked_response == mock_result
+        ), f"Response does not match for query: {url}, {query_params}"
