8053b21e3cf7a66689637c022352ece29c70783e
Stefan Furne <stefan@furne.net>
2025-11-18 19:32:57 +0100

    feat: implement some basic  debug logging

--- src/pxweb/__init__.py
+++ src/pxweb/__init__.py
@@ -1,3 +1,8 @@
+import logging
+from logging import NullHandler
+
 from .api import PxApi, get_known_apis
+
+logging.getLogger(__name__).addHandler(NullHandler())
 
 __all__ = ["PxApi", "get_known_apis"]
