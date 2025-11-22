8f7ca91f47592da33681953c02cc17b8a8d75507
Stefan Furne <stefan@furne.net>
2025-11-22 15:25:27 +0100

    feat: add nox

---
+++ noxfile.py
@@ -0,0 +1,13 @@
+from nox import Session, options
+from nox_uv import session
+
+options.default_venv_backend = "uv"
+
+
+@session(
+    name="tests",
+    python=["3.10", "3.11", "3.12", "3.13"],
+    uv_groups=["dev"],
+)
+def test(s: Session) -> None:
+    s.run("python", "-m", "pytest", "-vv")
