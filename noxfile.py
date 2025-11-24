560807dabbb006e68f08c66a91f7e8ced551ba8b
Stefan Furne <stefan@furne.net>
2025-11-24 21:19:21 +0100

    feat: add python 3.14

--- noxfile.py
+++ noxfile.py
@@ -6,7 +6,7 @@ options.default_venv_backend = "uv"
 
 @session(
     name="tests",
-    python=["3.10", "3.11", "3.12", "3.13"],
+    python=["3.10", "3.11", "3.12", "3.13", "3.14"],
     uv_groups=["dev"],
 )
 def test(s: Session) -> None:
