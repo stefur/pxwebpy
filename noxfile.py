47105ad70d72619fce514bc63631971a3b91811c
Stefan Furne <stefan@furne.net>
2026-06-14 21:59:19 +0200

    chore: clean up dependencies and split into groups

--- noxfile.py
+++ noxfile.py
@@ -7,7 +7,7 @@ options.default_venv_backend = "uv"
 @session(
     name="tests",
     python=["3.10", "3.11", "3.12", "3.13", "3.14"],
-    uv_groups=["dev"],
+    uv_groups=["tests"],
 )
 def test(s: Session) -> None:
     s.run("python", "-m", "pytest", "-vv")
